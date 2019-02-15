25f50fcdfc7ba7b0bb36d2d106001331d951fd65
joshua <javanmega0@gmail.com>
2019-02-15 12:47:11 +0300

    addition of a title

--- js/triangle.js
+++ js/triangle.js
@@ -1,7 +1,7 @@
 var one=prompt("enter a value of oneside of the desired triangle");
 var two=prompt("enter a value of  the second  side of the triangle desired");
 var three=prompt("enter a value of the third side of the triangle desired");
-if ( one + two <= three || two + three <= three || one + three <=)
+if ( one + two <= three || two + three <= three || one + three <=three)
  else if (one === tw0 && two === three && one === three) {
   alert ("equilateral triangle");
 }else if (one === two || two === three || one === three){
