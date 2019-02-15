4e554416263852de632b57e53fc1ae086f40a26e
joshua <javanmega0@gmail.com>
2019-02-15 12:06:29 +0300

    third variable

--- js/triangle.js
+++ js/triangle.js
@@ -1,2 +1,3 @@
 var one=prompt ("enter a value of oneside of the desired triangle");
 var two=prompt ("enter a value of  the second  side of the triangle desired");
+var three=prompt ("enter a value of the third side of the triangle desired")
