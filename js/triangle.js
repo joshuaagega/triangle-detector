3a6f20569f91e71434323eef79a41a1deddc53f4
joshua <javanmega0@gmail.com>
2019-02-15 12:13:18 +0300

    addition of else if

--- js/triangle.js
+++ js/triangle.js
@@ -3,4 +3,5 @@ var two=prompt ("enter a value of  the second  side of the triangle desired");
 var three=prompt ("enter a value of the third side of the triangle desired");
 if (one === tw0 && two===two && three===three) {
   alert ("equilateral triangle");
-}
+else if
+  } 
