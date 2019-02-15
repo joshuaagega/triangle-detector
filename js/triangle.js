79c345d6038a55f7129b94ebe486740b85d07839
joshua <javanmega0@gmail.com>
2019-02-15 12:44:27 +0300

    addition of isosles, equilateral,

--- js/triangle.js
+++ js/triangle.js
@@ -1,7 +1,10 @@
-var one=prompt ("enter a value of oneside of the desired triangle");
-var two=prompt ("enter a value of  the second  side of the triangle desired");
-var three=prompt ("enter a value of the third side of the triangle desired");
-if (one === tw0 && two===two && three===three) {
+var one=prompt("enter a value of oneside of the desired triangle");
+var two=prompt("enter a value of  the second  side of the triangle desired");
+var three=prompt("enter a value of the third side of the triangle desired");
+if ( one + two <= three || two + three <= three || one + three <=)
+ else if (one === tw0 && two === three && one === three) {
   alert ("equilateral triangle");
-else if
-  } 
+}else if (one === two || two === three || one === three){
+ alert ("isosles triangle");
+}
+else
