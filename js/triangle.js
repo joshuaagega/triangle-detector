0df2e761350990b84245a4b4661255342d6c5ebe
joshua <javanmega0@gmail.com>
2019-02-16 10:29:41 +0300

    spacing and indenting

--- js/triangle.js
+++ js/triangle.js
@@ -1,19 +1,19 @@
-function findOut(){
-var hair = document.getElementById('brain').value;
-var eye = document.getElementById('nose').value;
-var teeth = document.getElementById('tounge').value;
-alert(hair);
-var one = parseInt(hair);
-var two = parseInt(eye);
-var three = parseInt(teeth);
-alert(one);
-if ( one + two <= three || two + three <= three || one + three <= three){
- alert("not a triangle at all");
-}else if (one === two && two === three && one === three) {
- alert ("equilateral triangle");
-}else if (one != two && two != three && one != three){
- alert ("scalene triangle");
-}else {
- alert ("isosles triangle");
-}
+function findOut() {
+  var hair = document.getElementById('brain').value;
+  var eye = document.getElementById('nose').value;
+  var teeth = document.getElementById('tounge').value;
+  alert(hair);
+  var one = parseInt(hair);
+  var two = parseInt(eye);
+  var three = parseInt(teeth);
+  alert(one);
+  if (one + two <= three || two + three <= three || one + three <= three) {
+    alert("not a triangle at all");
+  } else if (one === two && two === three && one === three) {
+    alert("equilateral triangle");
+  } else if (one != two && two != three && one != three) {
+    alert("scalene triangle");
+  } else {
+    alert("isosles triangle");
+  }
 }
